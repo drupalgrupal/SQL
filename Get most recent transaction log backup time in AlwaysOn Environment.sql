a58fe91d06b2656a91416697d7132a69d295a5ff
Drupal <143964302+drupalgrupal@users.noreply.github.com>
2026-02-02 15:06:44 -0500

    Update Get most recent transaction log backup time in AlwaysOn Environment.sql

--- Get most recent transaction log backup time in AlwaysOn Environment.sql
+++ Get most recent transaction log backup time in AlwaysOn Environment.sql
@@ -1,28 +1,58 @@
--- See blog post: https://sqlpal.blogspot.com/2020/01/getting-most-recent-transaction-log.html
+-- See blog post:
+--   https://sqlpal.blogspot.com/2020/01/most-recent-sql-log-backup-time.html
+
+/*****************************************************************************************
+ Purpose:
+   Show, per database, when the last transaction log backup occurred and how much
+   log has accumulated since then, with Availability Group context where applicable.
+
+ Notes:
+   - Uses sys.dm_db_log_stats (SQL Server 2017+) to get last log backup time and
+     log size generated since that backup.
+   - Filters out SIMPLE recovery model databases because they do not use log backups.
+   - log_reuse_wait_desc gives you the reason log cannot be reused yet (if any).
+
+ Usage:
+   - Run in any database (master is fine).
+   - Optionally uncomment the WHERE clause warning line to highlight databases
+     that have not had a log backup in more than N minutes (e.g. 60*24 for 24 hours).
+
+******************************************************************************************/
 
 SELECT
-    D.NAME    DATABASE_NAME,
-    AG.NAME   AG_NAME,
-    DBL.LOG_BACKUP_TIME,
-    DATEDIFF(MINUTE, DBL.LOG_BACKUP_TIME, GETDATE()) MinutesSinceLastLogBackup,
-    HDRS.IS_PRIMARY_REPLICA,
-    DBL.RECOVERY_MODEL,
-    DBL.LOG_SINCE_LAST_LOG_BACKUP_MB,
-    D.STATE_DESC,
-    D.IS_READ_ONLY,
-    D.LOG_REUSE_WAIT_DESC,
-    DATABASEPROPERTYEX(D.NAME, 'Updateability') DATABASE_MODE
+    D.name  AS DATABASE_NAME, 
+    AG.name AS AG_NAME,                    -- If applicable
+    DBL.log_backup_time AS LOG_BACKUP_TIME,
+
+    -- How many minutes have elapsed since the last log backup.
+    DATEDIFF(MINUTE, DBL.log_backup_time, GETDATE()) AS MinutesSinceLastLogBackup,
+
+    HDRS.is_primary_replica,
+    DBL.recovery_model,
+
+    -- Approximate amount of log generated since the last log backup
+    DBL.log_since_last_log_backup_mb,
+
+    D.state_desc,
+    D.is_read_only,
+    D.log_reuse_wait_desc,
+    DATABASEPROPERTYEX(D.name, 'Updateability') AS DATABASE_MODE
 
 FROM
-    SYS.DATABASES D
-    LEFT JOIN SYS.DM_HADR_DATABASE_REPLICA_STATES HDRS 
-         ON HDRS.GROUP_DATABASE_ID = D.GROUP_DATABASE_ID
-            AND HDRS.REPLICA_ID = D.REPLICA_ID
-    LEFT JOIN SYS.AVAILABILITY_GROUPS AG ON AG.GROUP_ID = HDRS.GROUP_ID 
-    OUTER APPLY SYS.DM_DB_LOG_STATS ( D.DATABASE_ID )    DBL
+    sys.databases AS D
+    LEFT JOIN sys.dm_hadr_database_replica_states AS HDRS
+        ON HDRS.group_database_id = D.group_database_id
+       AND HDRS.replica_id        = D.replica_id
+    LEFT JOIN sys.availability_groups AS AG
+        ON AG.group_id = HDRS.group_id 
+    OUTER APPLY sys.dm_db_log_stats(D.database_id) AS DBL
 
-WHERE  1 = 1
-   AND DBL.RECOVERY_MODEL != 'SIMPLE'
---   AND DATEDIFF(MINUTE, DBL.LOG_BACKUP_TIME, GETDATE()) > 60*24
+WHERE
+      1 = 1
+  AND DBL.recovery_model <> 'SIMPLE'
+  -- Optional - Uncomment and adjust threshold as needed 
+  -- - (e.g., 60 for 1 hour, 60*4 for 4 hours, etc.).
+  -- AND DATEDIFF(MINUTE, DBL.log_backup_time, GETDATE()) > 60 * 24
 
-ORDER BY  DATABASE_NAME
+ORDER BY DATABASE_NAME;
+
