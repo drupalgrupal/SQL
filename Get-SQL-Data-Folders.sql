24ffa3be1a8ccf419792ca58fc82b9db97b85060
Drupal <143964302+drupalgrupal@users.noreply.github.com>
2026-02-02 15:31:48 -0500

    Update Get-SQL-Data-Folders.sql

--- Get-SQL-Data-Folders.sql
+++ Get-SQL-Data-Folders.sql
@@ -1,41 +1,80 @@
-/*
-RETURNS LIST OF FOLDER NAMES WHERE SQL SERVER DATABASE
-FILES ARE STORED
+/*****************************************************************************************
+ Script: Get SQL Server Database File Folder Locations
+ Purpose: Returns DISTINCT list of all folders containing SQL Server database files 
+          (data + log + FILESTREAM) across all databases on the instance.
 
-*/
+ Features:
+  * Works on SQL Server 2000-2025
+  * Generates mkdir commands for backup/restore prep
+  * Falls back to legacy method if VIEW SERVER STATE denied or old version
+
+ Usage: Run as sysadmin or user with VIEW SERVER STATE permission
+******************************************************************************************/
+
+SET NOCOUNT ON;
 
 DECLARE @PRODUCTVERSION NVARCHAR(132)
-DECLARE @PERMISSION_FLAG bit
-SET @PRODUCTVERSION = CAST(SERVERPROPERTY('ProductVersion') AS NVARCHAR(32)) 
-SET @PERMISSION_FLAG = 1
+DECLARE @PERMISSION_FLAG bit = 1  -- Set to 0 to force legacy path (testing)
 
--- For use on older versions of sql server or if you don't have permission to sys.master_files
--- tested in sql 2000 & 20005
-if SUBSTRING(@PRODUCTVERSION, 1, CHARINDEX('.', @PRODUCTVERSION)-1) < 10 OR @PERMISSION_FLAG = 0 OR @PERMISSION_FLAG IS NULL
-		BEGIN
-		PRINT 'We gonna use old school method.....'
-		set nocount on if object_id('tempdb..#t1') is not null drop table #t1
-			create table #t1 (fpath varchar(4000), fname varchar(8000))
-		insert into #t1
-			exec sp_msforeachdb 'select left(filename, len(filename) - charindex(''\'', reverse(rtrim(filename)))) fpath, filename fname from [?].dbo.sysfiles'
-		select distinct fpath from #t1 order by 1
+-- Get major version number (e.g. '15' for SQL 2019, '16' for 2022)
+SET @PRODUCTVERSION = CAST(SERVERPROPERTY('ProductVersion') AS NVARCHAR(32)) 
+
+-- Extract major version: everything before first decimal point
+DECLARE @MAJOR_VERSION INT = CAST(SUBSTRING(@PRODUCTVERSION, 1, CHARINDEX('.', @PRODUCTVERSION)-1) AS INT);
+
+-- LEGACY PATH: SQL Server 2000-2008R2 OR no VIEW SERVER STATE permission
+IF @MAJOR_VERSION < 10 OR @PERMISSION_FLAG = 0
+BEGIN
+    PRINT 'Using legacy method (SQL 2000-2008R2 compatibility or VIEW SERVER STATE denied)...';
+    
+    -- Clean up any existing temp table
+    IF OBJECT_ID('tempdb..#t1') IS NOT NULL 
+        DROP TABLE #t1;
+    
+    -- Temp table to hold folder paths and filenames from sysfiles
+    CREATE TABLE #t1 (
+        fpath   VARCHAR(4000),  -- Folder path only
+        fname   VARCHAR(8000)   -- Full filename for reference
+    );
+    
+    -- sp_MSforeachdb visits every DB on instance (undocumented but reliable)
+    INSERT INTO #t1 (fpath, fname)
+    EXEC sp_MSforeachdb '
+        SELECT 
+            LEFT(filename, LEN(filename) - CHARINDEX(''\\'', REVERSE(RTRIM(filename)))) AS fpath,
+            filename AS fname 
+        FROM [?].dbo.sysfiles
+    ';
+    
+    SELECT DISTINCT fpath AS folder_path
+    FROM #t1 
+    ORDER BY fpath;
+    
+    DROP TABLE #t1;
 END
 
--- For use on SQL version xxx and above 
+-- MODERN PATH: SQL Server 2012+ with sys.master_files (recommended)
 ELSE 
-		with cte as
-		(
-				select 
-					case 
-						when type = 2 then physical_name     -- FILESTREAM
-						else left(physical_name, len(physical_name) - charindex('\', reverse(rtrim(physical_name)))) 
-					end folder_path, 
-					physical_name  
-				from sys.master_files
-		)
-		select distinct 
-			folder_path,
-			[create_folder] = 'mkdir -p "' + folder_path + '"'
-		from cte
-                order by folder_path;
+BEGIN
+    PRINT 'Using modern sys.master_files method (SQL 2012+)...';
+    
+    WITH cte AS (
+        SELECT 
+            CASE 
+                -- FILESTREAM files (type=2) use full physical_name as "folder"
+                WHEN type = 2 THEN physical_name     
+                -- Regular files: strip filename to get folder path
+                ELSE LEFT(physical_name, 
+                         LEN(physical_name) - CHARINDEX('\', REVERSE(RTRIM(physical_name)))) 
+            END AS folder_path, 
+            physical_name  
+        FROM sys.master_files
+        WHERE type IN (0,1,2)  -- ROWS, LOG, FILESTREAM only
+    )
+    SELECT DISTINCT 
+        folder_path,
+        'mkdir -p "' + folder_path + '"' AS create_folder
+    FROM cte
+    ORDER BY folder_path;
+END
 
