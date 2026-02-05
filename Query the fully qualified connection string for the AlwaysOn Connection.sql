a50c6b6ecf816b7bfe96bafc5b3ce7cf15964752
Drupal <143964302+drupalgrupal@users.noreply.github.com>
2026-02-04 22:33:17 -0500

    Update Query the fully qualified connection string for the AlwaysOn Connection.sql

--- Query the fully qualified connection string for the AlwaysOn Connection.sql
+++ Query the fully qualified connection string for the AlwaysOn Connection.sql
@@ -1,50 +1,97 @@
--- See blog post: https://sqlpal.blogspot.com/2023/07/query-fully-qualified-connection-string.html
+/********************************************************************************************
+    Script: Get Fully Qualified SQL Connection String for Always On Databases
+    Author: [Your Name or Handle]
+    Reference: https://sqlpal.blogspot.com/2023/07/query-fully-qualified-connection-string.html
+    Purpose:
+        Returns a list of SQL Server connection strings for databases 
+        participating in an Always On Availability Group (AG).
 
-/* GET FULLY QUALIFIED SQL CONNECTION STRING FOR ALWAYSON DATABASE
+    Parameters:
+        @dbname NVARCHAR(500)
+            - If NULL or empty string, returns connection strings for *all* AG databases.
+            - If a specific database name is provided, validates that it exists 
+              and belongs to an AG.
 
-There is only one parameter, @dbname, if you are looking to get connection string 
-for a single database
+    Output Columns:
+        ConnString   : Fully qualified DNS\Instance connection string.
+        AGName       : Availability Group name.
+        DNSName      : Listener DNS name.
+        SQLInstance  : Current SQL instance name.
+        ClusterNodes : List of participating cluster node names.
+        TCPPort      : Listener port number.
 
-So, if the value for @dbname is null or an empty string, this script  will return 
-connection strings for all AGs defined in the current sql session
+    Notes:
+        - Requires VIEW SERVER STATE permission (to access DMVs).
+        - xp_regread requires sysadmin privileges (reads registry).
+********************************************************************************************/
 
-*/
+DECLARE @dbname NVARCHAR(500)
+SET @dbname = N''   -- Set to a specific database name if desired.
 
-declare @dbname nvarchar(500)
-set @dbname = ''
-
--- Read the domain name suffix for the current server that we will
--- append to the listener's DNS Name.
+-- STEP 1: Retrieve current server's domain name suffix from registry.
+-- This is used to append FQDN to the listener name.
 DECLARE @DomainName NVARCHAR(100)
-EXEC master.dbo.xp_regread 'HKEY_LOCAL_MACHINE', 'SYSTEM\CurrentControlSet\services\Tcpip\Parameters', N'Domain',@DomainName OUTPUT
+EXEC master.dbo.xp_regread 
+     'HKEY_LOCAL_MACHINE',
+     'SYSTEM\\CurrentControlSet\\Services\\Tcpip\\Parameters',
+     N'Domain',
+     @DomainName OUTPUT
 
--- throw an error if @dbname provided does not exist on the current sql server
-IF db_id(@dbname) is null and @dbname != ''   raiserror('No database found with name %s.', 16, 1, @dbname)
+-- STEP 2: Input validation.
+-- Ensure that the provided database exists locally.
+IF DB_ID(@dbname) IS NULL AND @dbname <> ''
+    RAISERROR('No database found with name %s.', 16, 1, @dbname)
 
--- check to see if the @dbname is an AG database
-ELSE IF db_id(@dbname) is not null and @dbname != ''   and NOT EXISTS (select * from sys.availability_databases_cluster where database_name = @dbname) 
-   raiserror('Database is not part of an Availability Group: %s.', 16, 1, @dbname)
+-- If the database exists but is not part of any Availability Group, raise an error.
+ELSE IF DB_ID(@dbname) IS NOT NULL AND @dbname <> '' 
+     AND NOT EXISTS (SELECT * FROM sys.availability_databases_cluster WHERE database_name = @dbname)
+    RAISERROR('Database is not part of an Availability Group: %s.', 16, 1, @dbname)
 
+-- STEP 3: Check if Always On is enabled before proceeding.
 ELSE IF SERVERPROPERTY('IsHadrEnabled') = 1
 BEGIN
-	DECLARE @ClusterNodes VARCHAR(8000) 
-	SELECT @ClusterNodes = COALESCE(@ClusterNodes + ', ', '') + node_name
-	FROM (select distinct node_name from sys.dm_hadr_availability_replica_cluster_nodes) a
+    -- Build a comma-separated list of all cluster node names for reference.
+    DECLARE @ClusterNodes VARCHAR(8000)
+    SELECT @ClusterNodes = COALESCE(@ClusterNodes + ', ', '') + node_name
+    FROM (SELECT DISTINCT node_name 
+          FROM sys.dm_hadr_availability_replica_cluster_nodes) AS a
 
-	select   UPPER(case when SERVERPROPERTY('InstanceName') is null then dns_name
-	           else CONCAT(dns_name, ISNULL(CONCAT('.', @DomainName), ''), '\', CAST(SERVERPROPERTY('InstanceName') as nvarchar(100)))  end) ConnString, 
-			 UPPER(ag.name) AGName,
-			 UPPER(dns_name) DNSName, 
-			 UPPER(CAST(SERVERPROPERTY('InstanceName')  as nvarchar(100))) SQLInstance, 
-			 UPPER(@ClusterNodes) ClusterNodes,
-			 port TCPPort
-			 from sys.availability_groups ag
-			 inner join sys.availability_group_listeners agl on ag.group_id = agl.group_id
-			 where cast(agl.group_id as varchar(100)) like case when @dbname is null or @dbname = '' then '%' 
-			       else (select cast(group_id as varchar(100)) from sys.availability_databases_cluster where database_name = @dbname) END
-
+    -- STEP 4: Generate and return connection info for each AG listener.
+    SELECT 
+        -- Construct the FQDN + Instance connection string.
+        UPPER(
+            CASE 
+                WHEN SERVERPROPERTY('InstanceName') IS NULL 
+                    THEN DNS_NAME 
+                ELSE CONCAT(
+                        DNS_NAME, 
+                        ISNULL(CONCAT('.', @DomainName), ''), 
+                        '\\', 
+                        CAST(SERVERPROPERTY('InstanceName') AS NVARCHAR(100))
+                     )
+            END
+        ) AS ConnString,
+        UPPER(ag.name) AS AGName,
+        UPPER(dns_name) AS DNSName,
+        UPPER(CAST(SERVERPROPERTY('InstanceName') AS NVARCHAR(100))) AS SQLInstance,
+        UPPER(@ClusterNodes) AS ClusterNodes,
+        port AS TCPPort
+    FROM sys.availability_groups AS ag
+    INNER JOIN sys.availability_group_listeners AS agl 
+        ON ag.group_id = agl.group_id
+    WHERE CAST(agl.group_id AS VARCHAR(100)) LIKE 
+          CASE 
+              WHEN @dbname IS NULL OR @dbname = '' THEN '%'
+              ELSE (
+                    SELECT CAST(group_id AS VARCHAR(100)) 
+                    FROM sys.availability_databases_cluster 
+                    WHERE database_name = @dbname
+                   )
+          END
 END
-ELSE IF SERVERPROPERTY('IsHadrEnabled') = 1 RAISERROR('SQL Server is not an AlwaysOn Cluster.', 16,1)
-ELSE
-	RAISERROR('Unknown error occurred.', 16,1)
 
+-- STEP 5: Handle case when Always On is not enabled.
+ELSE IF SERVERPROPERTY('IsHadrEnabled') = 0
+    RAISERROR('SQL Server is not an AlwaysOn Cluster.', 16, 1)
+ELSE
+    RAISERROR('Unknown error occurred.', 16, 1)
